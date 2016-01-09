f8d391e6d9e126d8d45bb959e83d15ca54a4b6e9
Black_Horse <heqingquan_tim@163.com>
2016-01-09 22:54:58 +0800

    Finsh this problem

--- Generate Parentheses.py
+++ Generate Parentheses.py
@@ -6,20 +6,20 @@ class Solution(object):
         """
         self.allParenthesis = []
         self.N = n
-        self.addParenthesis('',0)
-        return sorted(self.allParenthesis)
-    def addParenthesis(self,string,n):
-        if n == self.N:
+        self.addParenthesis('',n,n,0)
+        return self.allParenthesis
+        
+    def addParenthesis(self,string,lft,rht,n):
+        if n == 2*self.N:
             self.allParenthesis.append(string)
             return
-        self.addParenthesis('('+string+')',n+1)
-        if '('+string+')' != '()'+string:
-            if '()'+string == string+'()':
-                self.addParenthesis('()'+string,n+1)
-            else:
-                self.addParenthesis('()'+string,n+1)
-                self.addParenthesis(string+'()',n+1)
-
+        if lft == rht:
+            self.addParenthesis(string+'(',lft-1,rht,n+1)
+        if lft < rht:
+            if lft > 0:
+                self.addParenthesis(string+'(',lft-1,rht,n+1)
+            self.addParenthesis(string+")",lft,rht-1,n+1)
+       
 if __name__ == "__main__":
     obj = Solution()
     b = obj.generateParenthesis(4)
